379eb78effadab5141584e974e80958b574143ff
Midstackdev <midstackdev@gmail.com>
2020-07-24 15:10:32 +0000

    Layout with header and footer

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from './Hoc/Layout'
+
+const Routes = (props) => {
+  // console.log(props)
+  return(
+    <Layout>
+      hello
+    </Layout>
+  )
+}
+
+export default Routes;
